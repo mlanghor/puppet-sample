6069d56c98d586efaab711243bea7e35dec62e3a
Shane Meyers <shane@smugmug.com>
2015-02-25 12:06:33 -0800

    Escape char for cron

--- modules/puppet/manifests/cron.pp
+++ modules/puppet/manifests/cron.pp
@@ -2,7 +2,7 @@
 class puppet::cron {
     cron { 'twice-hourly puppet run':
         ensure => 'present',
-        command => 'sleep $(($RANDOM % 300)) && /usr/local/sbin/puppet-run.sh > /dev/null',
+        command => 'sleep $(($RANDOM \% 300)) && /usr/local/sbin/puppet-run.sh > /dev/null',
         minute => ['0', '30'],
         hour => '*',
         user => 'root',
